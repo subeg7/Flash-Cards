de3d0035f52b58667e6ba65bad02d7053cb6fd8a
Subeg7 <subeg7@gmail.com>
2019-06-23 18:07:14 +0545

    folder strucutre for App Screens

--- App.js
+++ App.js
@@ -21,8 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>FlashCards</Text>
+        <Text style={styles.instructions}>Easier way to Memorize</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
---
+++ src_checkpoint_01/components/Flashcards.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import Heading from "./Header";
+import DeckScreen from "./DeckScreen";
+import NewCardScreen from "./NewCardScreen";
+import ReviewScreen from "./ReviewScreen";
